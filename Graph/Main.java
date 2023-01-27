34da4d225b3ebe597f540281a7783b241310068a
ARSHAD <arshadkhanmak205@gmail.com>
2023-01-27 20:45:14 +0530

    working

--- Graph/Main.java
+++ Graph/Main.java
@@ -1,7 +1,23 @@
 package Graph;
 
-public class Main{
+import java.util.Scanner;
+
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("OK");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of vertices");
+        int edges = sc.nextInt();
+        System.out.println("Enter number of edegs");
+        int vertices = sc.nextInt();
+        creatingGraph graph = new creatingGraph(vertices);
+        System.out.println("Enter Edges");
+        for (int i = 0; i < edges; i++) {
+            System.out.println("Enter source vertex");
+            int source = sc.nextInt();
+            System.out.println("Enter destination vertex");
+            int destination = sc.nextInt();
+            graph.addEdge(source, destination);
+        }
     }
 }
